8bc5a9b25ceee81e2a1caa24a72b423be7567b00
satyarathi <jitendrasatyarathi@gmail.com>
2023-05-19 10:24:14 +0530

    [Jitendra]: Added Logger in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,18 +1,21 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require('../config/generateToken');
+const logger = require("../config/logger");
 
 const registerUser = asyncHandler(async(req,res) => {
     const { name, email, password, pic } =req.body;
 
     if(!name || !email || !password) {
         res.status(400);
+        logger.error("enter all required fields")
         throw new Error("Enter all the Required fields");
     }
 
     const userExists = await User.findOne({ email });
      if(userExists) {
         res.status(400);
+        logger.error("User already exists")
         throw new Error("User already exists");
      }
 
@@ -31,8 +34,10 @@ const registerUser = asyncHandler(async(req,res) => {
             pic: user.pic,
             token: generateToken(user._id)
         });
+        logger.info("Registration successfull")
      } else {
         res.status(400);
+        logger.error("error while registering user")
         throw new Error("Error in creating new User");
      }
 });
@@ -50,8 +55,11 @@ const authUser = asyncHandler(async(req,res) => {
            pic: user.pic,
            token: generateToken(user._id)
        });
+       logger.info("Login sucessfull")
+      
    } else {
        res.status(401);
+       logger.error("Invalid Credentials")
        throw new Error("Invalid email or password");
    }
 });
@@ -65,11 +73,9 @@ const allusers = asyncHandler(async(req,res) => {
   
       ]
     } : {};
-
-    //
-  
     const users = await User.find(keyword).find({_id:{$ne: req.user._id}});
     res.send(users);
+    logger.info("user found sucessfully")
   });
 
 module.exports = { registerUser,authUser, allusers}
